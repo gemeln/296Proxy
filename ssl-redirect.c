25c1bd53f012719bb9d35e8c730a746356955171
Jack Nash <jna407@icloud.com>
2021-04-19 09:42:27 -0500

    Added blocklist to ssl-redirect

--- ssl-redirect.c
+++ ssl-redirect.c
@@ -14,6 +14,21 @@
 #include "utils.c"
 #define BUFSIZE 1024 * 1024
 
+char* block_arr[100];
+int block_list_size = 0;
+
+int checkBlocklist(char* hostname) {
+    int is_blocked = 0;
+    for (int i = 0; i < block_list_size; i++) {
+        if (strncmp(block_arr[i], host, strlen(block_arr[i])) == 0) {
+            // website is blocked
+            return 1;
+        }
+    }
+
+    return 0;
+
+}
 void* HostToCli(void* args) {
     int* fds = (int*)args;
     int client_fd = fds[0];
@@ -31,6 +46,29 @@ void* HostToCli(void* args) {
 }
 
 int main(int argc, char** argv) {
+    
+    // Creating blocklist
+
+    FILE* block_file = fopen("blocked_list.txt", "r");
+    char* block_line = NULL;
+    size_t link_len = 0;
+    ssize_t bytes_read;
+    if (block_file != NULL) {
+        while ((bytes_read = getline(&block_line, &link_len, block_file)) != -1) {
+
+            block_line[strlen(block_line)-1] = '\0';
+            char* arr_str = malloc(strlen(block_line)+1);
+            strcpy(arr_str, block_line);
+            block_arr[block_list_size] = arr_str;
+            block_list_size++;
+        }
+    }
+
+    fclose(block_file);
+
+
+
+
     struct addrinfo hints, *result;
     memset(&hints, 0, sizeof(struct addrinfo));
     hints.ai_family = AF_INET;
@@ -65,6 +103,17 @@ int main(int argc, char** argv) {
     hostinfo inf;
     parseHost(buffer, &inf);
     printf("%s %s\n", inf.hostname, inf.port);
+
+    int is_blocked = checkBlocklist(inf.hostname);
+    if (is_blocked) {
+        char* mesg = "403 Forbidden\r\n\r\n";
+        write_all_to_socket(client_fd, msg, strlen(mesg));
+        puts("Sent 403 Forbidden to client");
+        shutdown(client_fd, SHUT_RDWR);
+        close(client_fd);
+        return 0;
+    }
+
     int dest_fd = connect_to_server(inf.hostname, inf.port);
     assert(dest_fd >= 0);
     // Success, return 200
