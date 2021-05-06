d1e8945eb4512d7546aac8b8c23f8a244aed6ae5
Matthew Tang <mhtang2@illinois.edu>
2021-05-06 00:59:57 +0000

    Container added file system

--- Containers/container.c
+++ Containers/container.c
@@ -4,28 +4,62 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <signal.h>
 #include <string.h>
 #include "includes/vector.h"
 #include <sys/syscall.h>
-
+#include <errno.h>
+#include <sys/mount.h>
+#include <sys/stat.h>
+#include <assert.h>
 static char child_stack[1048576];
-
 static vector* containers;
 
+void handleSigint(int sig) {
+    for (size_t i = 0;i < vector_size(containers);++i) {
+        pid_t proc = *((int*)vector_get(containers, i));
+        printf("Waiting on process %d\n", proc);
+        int stat;
+        waitpid(proc, &stat, 0);
+        if (WIFEXITED(stat)) {
+            printf("process %d exited\n", proc);
+        }
+    }
+    rmdir("./busybox/.old");
+    vector_destroy(containers);
+    exit(0);
+}
+int pivot_root(char* a, char* b) {
+    if (mount(a, a, "bind", MS_BIND | MS_REC, "") < 0) {
+        printf("error mount: %s\n", strerror(errno));
+    }
+    if (mkdir(b, 0755) < 0) {
+        printf("error mkdir %s\n", strerror(errno));
+    }
+    printf("pivot setup ok\n");
+    return syscall(SYS_pivot_root, a, b);
+}
 int startContainer(void* args) {
-    long res = syscall(SYS_pivot_root, "./tmp1/", "./tmp2/");
-    if (res == -1)
-        perror("pivot");
-    char* command = args;
-    free(command);
-    printf("PID: %ld\n", (long)getpid());
+    pivot_root("./busybox", "./busybox/.old");
+    mount("tmpfs", "/dev", "tmpfs", MS_NOSUID | MS_STRICTATIME, NULL);
+    mount("proc", "/proc", "proc", 0, NULL);
+    umount2("/.old", MNT_DETACH);
+
     chdir("/");
-    system("echo hi > hi.txt");
+    // system("ifconfig");
+    // printf("PID: %ld\n", (long)getpid());
+    // system("./net.sh eth0");
+    // system("echo \"nameserver 8.8.8.8\" >> /etc/resolv.conf");
+    system(args);
     return -1;
 }
+#define MAX_PROCESS 10
+
 
 int main() {
-    puts("HI");
+    system("mount --make-rprivate  /");
+    signal(SIGINT, handleSigint);
+    containers = int_vector_create();
     char* cmd = NULL;
     size_t cmdSize = 0;
     containers = int_vector_create();
@@ -33,8 +67,14 @@ int main() {
         printf("New container: ");
         size_t linesize = getline(&cmd, &cmdSize, stdin);
         cmd[linesize - 1] = 0;
-        pid_t child_pid = clone(startContainer, child_stack + 1048576, CLONE_NEWPID | SIGCHLD | CLONE_NEWNS, strdup(cmd));
+        if (vector_size(containers) == MAX_PROCESS) {
+            fprintf(stderr, "Exceeded max processes: %d\n", MAX_PROCESS);
+            continue;
+        }
+        pid_t child_pid = clone(startContainer, child_stack + 1048576, CLONE_NEWPID | SIGCHLD | CLONE_NEWNS | CLONE_NEWNET, cmd);
         printf("clone() = %ld\n", (long)child_pid);
+        vector_push_back(containers, &child_pid);
     }
+
     return 0;
 }
